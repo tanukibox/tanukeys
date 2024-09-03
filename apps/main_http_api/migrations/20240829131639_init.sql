6e84496b39652b0e573c2917fcce1ae5a2cd3805
Javier Parada <javierparada@pm.me>
2024-09-03 13:23:48 +0200

    add cryptokeys DB schema

--- apps/main_http_api/migrations/20240829131639_init.sql
+++ apps/main_http_api/migrations/20240829131639_init.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS users (
 
     _created_at timestamp with time zone DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS cryptokeys (
+    id text PRIMARY KEY,
+    name text NOT NULL,
+    payload text NOT NULL,
+    user_id text NOT NULL,
+
+    _created_at timestamp with time zone DEFAULT now()
+);
