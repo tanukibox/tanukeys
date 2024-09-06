3aa9898b71c09d78b00a82e495fe2d83326ce1f5
Javier Parada <javierparada@pm.me>
2024-09-06 22:30:51 +0200

    remove old error types

--- libs/kernel/src/crypto_keys/domain/errors/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod crypto_key_already_exists_error;
-pub mod crypto_key_not_found_error;
-pub mod crypto_key_errors;
--- libs/kernel/src/crypto_keys/domain/mod.rs
+++ libs/kernel/src/crypto_keys/domain/mod.rs
@@ -1,4 +1,3 @@
 pub mod entities;
-pub mod errors;
 pub mod events;
 pub mod crypto_key_repository;
