7fa63b8b75faa2195fea70f6d2e6d35fb7d9b06a
Javier Parada <javierparada@pm.me>
2024-09-16 12:46:53 +0200

    fix crupto key by user

--- apps/main_http_api/src/v1/crypto_keys/crypto_key_get_controller.rs
+++ apps/main_http_api/src/v1/crypto_keys/crypto_key_get_controller.rs
@@ -1,8 +1,10 @@
 
 use actix_web::{web, HttpRequest, HttpResponse};
-use kernel::crypto_keys::application::find_many_by_user::crypto_keys_by_user_finder::CryptoKeysByUserFinder;
+use kernel::shared::domain::errors::DomainError;
 use serde::Deserialize;
+use kernel::crypto_keys::application::find_one::crypto_key_finder::CryptoKeyFinder;
 use kernel::crypto_keys::domain::crypto_key_repository::CryptoKeyRepository;
+use kernel::crypto_keys::domain::entities::crypto_key_id::CryptoKeyId;
 use kernel::crypto_keys::infrastructure::dtos::crypto_key_json_dto::parse_to_dto;
 use kernel::shared::domain::entities::user_id::UserId;
 
@@ -12,25 +14,32 @@ pub struct Params {
 }
 
 pub(crate) async fn controller<R: CryptoKeyRepository>(
+    crypto_key_id: web::Path<String>,
     req: HttpRequest,
-    finder: web::Data<CryptoKeysByUserFinder<R>>,
+    finder: web::Data<CryptoKeyFinder<R>>,
 ) -> HttpResponse {
     let params = web::Query::<Params>::from_query(req.query_string())
         .unwrap_or(web::Query(Params { user_id: String::from("") }));
     let user_id = params.user_id.clone();
     let user_id = UserId::new(user_id.parse().unwrap());
-    if user_id.is_err() {
+    let crypto_key_id = CryptoKeyId::new(crypto_key_id.parse().unwrap());
+    if user_id.is_err() || crypto_key_id.is_err() {
         return HttpResponse::BadRequest().finish()
     }
-
-    let res = finder.run(user_id.unwrap()).await;
+    let res = finder.run(crypto_key_id.unwrap(), user_id.unwrap()).await;
     match res {
-        Ok(keys) => {
-            let dtos: Vec<_> = keys.iter().map(|key| parse_to_dto(&key)).collect();
-            HttpResponse::Ok().json(dtos)
+        Ok(key) => {
+            let dto = parse_to_dto(&key);
+            HttpResponse::Ok().json(dto)
         }
         Err(err) => {
             match err {
+                DomainError::CryptoKeyNotFound { id, user_id } => {
+                    HttpResponse::NotFound().body(format!("Crypto key with id <{}>, not found for user <{}>.", id, user_id))
+                },
+                DomainError::CryptoKeyAlreadyExists { id, user_id } => {
+                    HttpResponse::Conflict().body(format!("Crypto key with id <{}>, already exists for user <{}>.", id, user_id))
+                },
                 _ => HttpResponse::InternalServerError().finish()
             }
         }
--- apps/main_http_api/src/v1/crypto_keys/mod.rs
+++ apps/main_http_api/src/v1/crypto_keys/mod.rs
@@ -4,11 +4,12 @@ use events::domain::event_bus::EventBus;
 use kernel::crypto_keys::domain::crypto_key_repository::CryptoKeyRepository;
 mod crypto_key_post_controller;
 mod crypto_key_get_controller;
+mod crypto_keys_get_controller;
 
 pub fn router<R: CryptoKeyRepository, E: EventBus>(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("/api/v1/cryptokeys")
-            .configure(|cfg: &mut ServiceConfig| -> () { cfg.route("/", web::get().to(crypto_key_get_controller::controller::<R>)); })
+            .configure(|cfg: &mut ServiceConfig| -> () { cfg.route("/", web::get().to(crypto_keys_get_controller::controller::<R>)); })
             .configure(|cfg: &mut ServiceConfig| -> () { cfg.route("/{crypto_key_id}", web::get().to(crypto_key_get_controller::controller::<R>)); })
             .configure(|cfg: &mut ServiceConfig| -> () { cfg.route("/", web::post().to(crypto_key_post_controller::controller::<R, E>)); })
     );
