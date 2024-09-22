05659499c6618b7b2546f45041c456414e0efb21
Javier Parada <javierparada@pm.me>
2024-09-22 12:22:23 +0200

    Update first test

--- libs/kernel/src/users/domain/entities/tests/mod.rs
+++ libs/kernel/src/users/domain/entities/tests/mod.rs
@@ -2,18 +2,11 @@ pub mod mothers;
 
 #[cfg(test)]
 mod create_simple_user {
-    use crate::shared::domain::entities::user_id::UserId;
 
-    use super::super::user::User;
-    use super::super::user_name::UserName;
-    use super::super::user_bio::UserBio;
+    use super::mothers::user_mother::UserMother;
 
     #[test]
     fn create_simple_user() {
-        let id = UserId::new("".to_string()).unwrap();
-        let name = UserName::new("tanukeys".to_string()).unwrap();
-        let bio = UserBio::new(Some("I am a software engineer.".to_string())).unwrap();
-
-        User::new(id, name, bio);
+        UserMother::random();
     }
 }
