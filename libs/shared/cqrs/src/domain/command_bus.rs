14aa7330339166d7976fb5f30dd916a182b86d10
Javier Parada <javierparada@pm.me>
2024-10-01 15:37:55 +0200

    update command bus

--- libs/shared/cqrs/src/domain/command_bus.rs
+++ libs/shared/cqrs/src/domain/command_bus.rs
@@ -1,7 +1,12 @@
-use super::{command::Command, command_bus_response::CommandBusResponse};
+use std::sync::Arc;
+
+use async_trait::async_trait;
+
+use super::{command::Command, command_bus_response::CommandBusResponse, command_handler::CommandHandler};
 
 
-
+#[async_trait]
 pub trait CommandBus {
-    fn dispatch(&self, command: Box<dyn Command>) -> Box<dyn CommandBusResponse>;
+    fn register(&mut self, handler: Arc<dyn CommandHandler>);
+    async fn dispatch(&self, command: Box<dyn Command>) -> Box<dyn CommandBusResponse>;
 }
