772c784351e8a50e1ffc27636baa0a0593f3e665
Javier Parada <javierparada@pm.me>
2024-10-04 12:52:50 +0200

    fix user tests

--- tests/libs/kernel/src/users/user_id_tests.rs
+++ tests/libs/kernel/src/users/user_id_tests.rs
@@ -12,67 +12,67 @@ pub mod user_id {
 
     #[test]
     fn create_empty_id() {
-        let res = UserId::new("".to_string());
+        let res = UserId::new(Some("".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_with_special_chars() {
-        let res = UserId::new("tanuki_box".to_string());
+        let res = UserId::new(Some("tanuki_box".to_string()));
         assert!(res.is_ok())
     }
 
     #[test]
     fn create_with_one_upper_case_id() {
-        let res = UserId::new("tanukiBox".to_string());
+        let res = UserId::new(Some("tanukiBox".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_with_two_upper_case_id() {
-        let res = UserId::new("TanukiBox".to_string());
+        let res = UserId::new(Some("TanukiBox".to_string()));
         assert!(res.is_err())
     }
     
     #[test]
     fn create_all_upper_case_id() {
-        let res = UserId::new("TANUKIBOX".to_string());
+        let res = UserId::new(Some("TANUKIBOX".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_with_special_char() {
-        let res = UserId::new("tanuki_box".to_string());
+        let res = UserId::new(Some("tanuki_box".to_string()));
         assert!(res.is_ok())
     }
 
     #[test]
     fn create_with_just_not_allowed_char() {
-        let res = UserId::new(";".to_string());
+        let res = UserId::new(Some(";".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_with_not_allowed_char() {
-        let res = UserId::new("tanuki;box".to_string());
+        let res = UserId::new(Some("tanuki;box".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_just_with_space_char() {
-        let res = UserId::new(" ".to_string());
+        let res = UserId::new(Some(" ".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_with_space_char() {
-        let res = UserId::new("tanuki box".to_string());
+        let res = UserId::new(Some("tanuki box".to_string()));
         assert!(res.is_err())
     }
 
     #[test]
     fn create_with_some_not_allowed_char() {
-        let res = UserId::new("tanuki box!".to_string());
+        let res = UserId::new(Some("tanuki box!".to_string()));
         assert!(res.is_err())
     }
 
