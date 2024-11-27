31a4e39a6ae37cc8f04b17f0615c17186ee398e1
Javier Parada <javierparada@pm.me>
2024-11-27 10:35:09 +0100

    add inmemory command bus impl

--- libs/shared/cqrs/src/domain/command_handler.rs
+++ libs/shared/cqrs/src/domain/command_handler.rs
@@ -3,4 +3,5 @@ use super::{command::Command, command_bus_response::CommandBusResponse};
 
 pub trait CommandHandler {
     fn handle(&self, command: Box<dyn Command>) -> Box<dyn CommandBusResponse>;
+    fn subscribet_to(&self) -> String;
 }
