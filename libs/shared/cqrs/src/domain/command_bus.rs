9c4e3ece3fb328fd2ad0639e60f2023316bc5a49
Javier Parada <javierparada@pm.me>
2024-11-27 10:33:49 +0100

    add inmemory command bus impl

--- libs/shared/cqrs/src/domain/command_bus.rs
+++ libs/shared/cqrs/src/domain/command_bus.rs
@@ -1,7 +1,10 @@
-use super::{command::Command, command_bus_response::CommandBusResponse};
+use std::sync::Arc;
+
+use super::{command::Command, command_bus_response::CommandBusResponse, command_handler::CommandHandler};
 
 
 
 pub trait CommandBus {
+    fn register(&mut self, handler: Arc<dyn CommandHandler>);
     fn dispatch(&self, command: Box<dyn Command>) -> Box<dyn CommandBusResponse>;
 }
