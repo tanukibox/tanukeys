95eea7147351bdb67c632342b9d309a81e5c115f
Javier Parada <javierparada@pm.me>
2025-03-05 18:17:58 +0100

    delete unused methods

---
+++ libs/kernel/src/subscriptions/domain/subscription_repository.rs
@@ -0,0 +1,13 @@
+use crate::subscriptions::domain::entities::subscription::Subscription;
+use crate::shared::domain::entities::user_id::UserId;
+use crate::shared::domain::errors::DomainError;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait SubscriptionRepository: Send + Sync + 'static {
+    async fn find_many(&self, user_id: &UserId) -> Result<Vec<Subscription>, DomainError>;
+    async fn find_by_domain(&self, user_id: &UserId, domain: &str) -> Result<Subscription, DomainError>;
+    async fn create_one(&self, subscription: &Subscription) -> Result<(), DomainError>;
+    async fn update_one(&self, subscription: &Subscription) -> Result<(), DomainError>;
+    async fn delete_one(&self, user_id: &UserId, domain: &str) -> Result<(), DomainError>;
+} 
