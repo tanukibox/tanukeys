5f091c31780ee883df414b3ca3b55707a389a947
Javier Parada <javierparada@pm.me>
2025-03-05 18:20:54 +0100

    remove docs

--- libs/kernel/src/subscriptions/domain/entities/external_domain.rs
+++ libs/kernel/src/subscriptions/domain/entities/external_domain.rs
@@ -1,8 +1,3 @@
-/// Value object representing a subscription's external domain.
-/// 
-/// This value object ensures that external domains are not empty and provides
-/// a type-safe way to handle external domain information.
-
 use crate::shared::domain::errors::DomainError;
 use std::hash::{Hash, Hasher};
 
@@ -12,16 +7,6 @@ pub struct ExternalDomain {
 }
 
 impl ExternalDomain {
-    /// Creates a new external domain.
-    /// 
-    /// # Arguments
-    /// 
-    /// * `val` - The string value of the external domain
-    /// 
-    /// # Returns
-    /// 
-    /// A `Result` containing either a new `ExternalDomain` or a `DomainError`
-    /// if the input is empty.
     pub fn new(val: String) -> Result<Self, DomainError> {
         if val.is_empty() {
             return Err(DomainError::ValueObjectError { 
@@ -31,7 +16,6 @@ impl ExternalDomain {
         Ok(Self { value: val })
     }
 
-    /// Returns the string value of the external domain.
     pub fn value(&self) -> String {
         self.value.clone()
     }
