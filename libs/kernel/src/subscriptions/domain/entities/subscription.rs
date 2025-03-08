67932a723f1b1ad21b4b292c41b99df163bfe9d5
Javier Parada <javierparada@pm.me>
2025-03-08 06:02:06 +0100

    add crypto key id to suscription

--- libs/kernel/src/subscriptions/domain/entities/subscription.rs
+++ libs/kernel/src/subscriptions/domain/entities/subscription.rs
@@ -1,5 +1,8 @@
 use crate::shared::domain::entities::user_id::UserId;
-use crate::crypto_keys::domain::entities::crypto_key_domain::CryptoKeyDomain;
+use crate::crypto_keys::domain::entities::{
+    crypto_key_domain::CryptoKeyDomain,
+    crypto_key_id::CryptoKeyId,
+};
 use crate::subscriptions::domain::entities::external_domain::ExternalDomain;
 use aggregate_root::domain::aggregate_root::AggregateRoot;
 
@@ -8,6 +11,7 @@ pub struct Subscription {
     pub user_id: UserId,
     pub key_domain: CryptoKeyDomain,
     pub external_domain: ExternalDomain,
+    pub crypto_key_id: CryptoKeyId,
 }
 
 impl Subscription {
@@ -15,11 +19,13 @@ impl Subscription {
         user_id: UserId,
         key_domain: CryptoKeyDomain,
         external_domain: ExternalDomain,
+        crypto_key_id: CryptoKeyId,
     ) -> Self {
         Self {
             user_id,
             key_domain,
             external_domain,
+            crypto_key_id,
         }
     }
 }
@@ -36,6 +42,7 @@ impl Clone for Subscription {
             self.user_id.clone(),
             self.key_domain.clone(),
             self.external_domain.clone(),
+            self.crypto_key_id.clone(),
         )
     }
 }
@@ -44,7 +51,8 @@ impl PartialEq for Subscription {
     fn eq(&self, other: &Self) -> bool {
         self.user_id.value() == other.user_id.value() &&
         self.key_domain.value() == other.key_domain.value() &&
-        self.external_domain.value() == other.external_domain.value()
+        self.external_domain.value() == other.external_domain.value() &&
+        self.crypto_key_id.value() == other.crypto_key_id.value()
     }
 }
 
